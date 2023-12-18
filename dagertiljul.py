e79f51350eb9f0c932dd314a051430a59c749983
Raydog69 <trygve@garatun.no>
2023-12-18 11:43:08 +0100

    p

--- dagertiljul.py
+++ dagertiljul.py
@@ -1,6 +1,16 @@
-from datetime import datetime
+import datetime
+import pandas as pd
 
-dateToday = datetime.today().strftime('%d')
+yearToday = int(datetime.datetime.now().year)
+monthToday = int(datetime.datetime.now().month)
+dateToday = int(datetime.datetime.now().day)
 
-print(24 - int(dateToday))
+daysToChrismas = pd.Timestamp(2021, 12, 24).dayofyear
+dayToday = pd.Timestamp(yearToday, monthToday, dateToday).dayofyear
 
+if dayToday > daysToChrismas:
+    print(f'Det er {daysToChrismas + (pd.Timestamp(yearToday + 1, 12, 31).dayofyear - dayToday)} dager til jul')
+elif dayToday < daysToChrismas:
+    print(f'Det er {daysToChrismas - dayToday} dager til jul')
+elif dayToday == daysToChrismas:
+    print("Det er Jul")
