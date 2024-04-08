deb496af695c69744254405b97465e0a2cfc3ed0
Raydog69 <trygve@garatun.no>
2024-04-08 08:40:48 +0200

    p

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Bye World")
+def f(x):
+    return 5*(x-1)
+
+def find(a1, n):
+    an = a1
+    for i in range(1, n + 1):
+        if i == 1:
+            an == 1
+        else:
+            an += f(i)
+    return an
+
+print(find(1, 2))
